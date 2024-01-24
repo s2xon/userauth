84c77d6247762c0b1e3ae89f76b9c25577537501
Saxon Payne <saxattack101@gmail.com>
2024-01-24 10:58:42 -0800

    half way completed

--- server/main.go
+++ server/main.go
@@ -4,21 +4,39 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
-	// "root/server/api"
+	"os"
+	"root/server/api"
+
+    "gorm.io/driver/mysql"
+	"github.com/joho/godotenv"
+	"gorm.io/gorm"
 )
+
+
 
 const port = "localhost:3000"
 
 type Data struct {
-    Email     string    `json:"Email"`
-	Password  string `json:"Password"`
+    ID        int       `gorm:"column:ID;primaryKey;autoIncrement"`
+    Email     string    `gorm:"column:email"`
+    Password  string   `gorm:"column:password"`
 }
 
 
 func main() {
+    if err := godotenv.Load(); err != nil {
+		log.Fatalf("failed to load environment variables: %v", err)
+	}
 
-	
+	// Connect to PlanetScale database using DSN environment variable.
+	db, err := gorm.Open(mysql.Open(os.Getenv("DSN")), &gorm.Config{
+		DisableForeignKeyConstraintWhenMigrating: true,
+	})
+	if err != nil {
+		log.Fatalf("failed to connect to PlanetScale: %v", err)
+	}
 
+    
     http.HandleFunc("/", func (w http.ResponseWriter, r *http.Request) {
         if r.Method == http.MethodPost {
             data := &Data{}
@@ -28,10 +46,29 @@ func main() {
             }
             log.Println("Got Post:", data.Email)
             log.Println("Got Post:", data.Password)
-        } 
+
+            hashedEmail, err := en.HashPassword(data.Email)
+            if err != nil {
+                log.Fatal(err)
+            }
+
+            hashedPass, err := en.HashPassword(data.Password)
+            if err != nil {
+                log.Fatal(err)
+            }
+
+            // Convert hashed password to []byte
+           
+
+            db.Save(&Data{Email: hashedEmail, Password: hashedPass})
+            
+
+
+        }
             http.ServeFile(w, r, "./src/public/index.html")
     })
 	
     http.ListenAndServe(port, nil)
 }
+
 
